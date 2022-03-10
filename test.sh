502fff6f8b12a458b8571504ef234e33c12bfaa8
Žiga Leber <zigaleber7@gmail.com>
2022-03-10 15:24:52 +0100

    Add positive and negative examples

--- test.sh
+++ test.sh
@@ -9,19 +9,32 @@ declare -r path=$(dirname $(realpath "$0"))
 
 declare -i err=0
 
-for file in "$path"/examples/example{3..100}.txt
+for file in "$path"/positive/example{3..100}.txt
 do
     echo "$file"
     cat "$file"
     declare output=$(./run.sh "$file")
-    declare tokens="${file%.*}.tokens"
 
-    if [[ "$output" == $(cat "$tokens") ]]
+    if [[ "$output" == "accept" ]]
     then
-        echo "[OK]"
+        echo -e "\n[OK]"
     else
-        wdiff <(cat - <<<"$output") "$tokens"
-        newline
+        echo -e "\n[FAIL]"
+        ((err++))
+    fi
+done
+
+for file in "$path"/negative/example{4..100}.txt
+do
+    echo "$file"
+    cat "$file"
+    declare output=$(./run.sh "$file")
+
+    if [[ "$output" == "reject" ]]
+    then
+        echo -e "\n[OK]"
+    else
+        echo -e "\n[FAIL]"
         ((err++))
     fi
 done
